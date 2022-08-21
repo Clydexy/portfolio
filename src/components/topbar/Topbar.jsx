2701352bec1e003dfcd945948a4a67ff59beffbd
Clydexy <xuj1@kgv.hk>
2022-08-21 23:27:48 +0800

    Animated Hamburger Website

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./topbar.scss"
 import {Phone, Mail} from '@material-ui/icons';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">brilliance.</a>
@@ -17,7 +17,11 @@ export default function Topbar() {
           </div>
         </div>
         <div className="right">
-          This is right
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
