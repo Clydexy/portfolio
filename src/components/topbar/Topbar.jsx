40306a8ab024ce8c9fb59fe6d1f54f502c4dac29
Clydexy <xuj1@kgv.hk>
2022-08-21 21:56:49 +0800

    Changed Phone Icon

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,5 @@
 import "./topbar.scss"
-import {Person, Mail} from '@material-ui/icons';
+import {Phone, Mail} from '@material-ui/icons';
 
 export default function Topbar() {
   return (
@@ -8,7 +8,7 @@ export default function Topbar() {
         <div className="left">
           <a href="#intro" className="logo">brilliance.</a>
           <div className="itemContainer">
-            <Person className="icon"/>
+            <Phone className="icon"/>
             <span>+852 5427 2943</span>
           </div>
           <div className="itemContainer">
