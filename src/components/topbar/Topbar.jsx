2a0b84510736ef5d31793c83989c2da2002e56f9
Clydexy <xuj1@kgv.hk>
2022-08-21 21:06:05 +0800

    Topbar Complete

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,25 @@
 import "./topbar.scss"
+import {Person, Mail} from '@material-ui/icons';
 
 export default function Topbar() {
   return (
     <div className="topbar">
-
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">brilliance.</a>
+          <div className="itemContainer">
+            <Person className="icon"/>
+            <span>+852 5427 2943</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon"/>
+            <span>xuj1@kgv.hk</span>
+          </div>
+        </div>
+        <div className="right">
+          This is right
+        </div>
+      </div>
     </div>
   )
 }
