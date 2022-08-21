b042dfe3b9a5270cb197516d77864388ed471002
Clydexy <xuj1@kgv.hk>
2022-08-21 21:44:05 +0800

    Changed homepage

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-        
+        <h1>Portfolio</h1>
+        <ul>
+          <li>Featured</li>
+          <li>Projects</li>
+          <li>Cinematography</li>
+          <li>Web and App Development</li>
+          <li>Community Service</li>
+        </ul>
+        <div className="container">
+          <div className="item">
+            <img src="" alt=""/>
+            <h3></h3>
+          </div>
+        </div>
     </div>
   )
 }
