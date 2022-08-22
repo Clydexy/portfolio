b6706bda979df4390ec51973edca467081b25ee3
Clydexy <xuj1@kgv.hk>
2022-08-22 23:13:44 +0800

    Finished Works Template

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
 export const featuredPortfolio = [
     {
       id: 1,
-      title: "Social Media App",
+      title: "Teen Drama",
       img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+        "assets/TeenDrama.png",
     },
     {
       id: 2,
