0fabdc3984656902ff719c65112295d0a447a65c
Clydexy <xuj1@kgv.hk>
2022-08-22 17:45:01 +0800

    Finished Portfolio Template

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,20 +1,76 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss"
+import {
+  featuredPortfolio,
+  projectsPortfolio,
+  cinematographyPortfolio,
+  webappPortfolio,
+  communityPortfolio,
+} from "../../data";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured")
+  const [data, setData] = useState([])
+  const list =[
+    {
+        id: "featured",
+        title: "Featured",
+    },
+    {
+        id: "projects",
+        title: "Projects",
+    },
+    {
+        id: "cinematography",
+        title: "Cinematography",
+    },
+    {
+        id: "webapp",
+        title: "Web and App Development",
+    },
+    {
+        id: "community",
+        title: "Community Service",
+    },
+];
+
+useEffect(() => {
+  switch(selected){
+    case "featured":
+      setData(featuredPortfolio)
+      break
+    case "projects":
+      setData(projectsPortfolio)
+      break
+    case "cinematography":
+      setData(cinematographyPortfolio)
+      break
+    case "webapp":
+      setData(webappPortfolio)
+      break
+    case "community":
+      setData(communityPortfolio)
+      break
+    default:
+      setData(featuredPortfolio)
+  }
+}, [selected])
   return (
     <div className="portfolio" id="portfolio">
         <h1>Portfolio</h1>
         <ul>
-          <li>Featured</li>
-          <li>Projects</li>
-          <li>Cinematography</li>
-          <li>Web and App Development</li>
-          <li>Community Service</li>
+          {list.map(item=>(
+            <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
+          ))}
         </ul>
         <div className="container">
-          <div className="item">
-            <img src="" alt=""/>
+          {data.map((d)=>(
+            <div className="item">
+            <img src={d.img} alt=""/>
+            <h3>{d.title}</h3>
           </div>
+          ))}
         </div>
     </div>
   )
