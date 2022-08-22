c5dd51d6dd8fa4f575525d0323f09aaa5a9716fe
Clydexy <xuj1@kgv.hk>
2022-08-22 16:20:12 +0800

    Custom Intro Image

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -15,7 +15,7 @@ export default function Intro() {
     <div className="intro" id="intro">
         <div className="left">
           <div className="imgContainer">
-            <img src="assets/man.png" alt=""/>
+            <img src="assets/portrait.png" alt=""/>
           </div>
         </div>
         <div className="right">
